d80b73b6274869c6b4c5dc911a357e31e4998a60
tamanstoria <hertasning.rauf@gmail.com>
2026-08-22 16:04:02 +0800

    Update pengaturan.js

--- pengaturan.js
+++ pengaturan.js
@@ -5,9 +5,9 @@
 
 const CONFIG_APP = {
     // 1. Nama Studio Foto Anda (Akan tampil di header dan portal klien)
-    namaVendor: "Portal Client",
+    namaVendor: "Taman Storia",
 
     // 2. Google Drive API Key (Penting agar galeri bisa memuat foto)
     googleApiKey: "AIzaSyAZczbLeVTXl-QKqLDJFWQLCd-lW9jqqxo",
-    whatsappAdmin: "62821000000000" // <-- TAMBAHKAN BARIS INI (Ganti dengan nomor WA admin)
+    whatsappAdmin: "082156210121" // <-- TAMBAHKAN BARIS INI (Ganti dengan nomor WA admin)
 };
